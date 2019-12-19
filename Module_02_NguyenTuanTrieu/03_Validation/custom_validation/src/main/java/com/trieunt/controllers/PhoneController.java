ad3759bbf968ac95f77b88fcc24cae2660015276
trieunt-1995 <trieuntse1995@gmail.com>
2019-12-18 18:48:17 -0800

    Add folder Validation

---
+++ Module_02_NguyenTuanTrieu/03_Validation/custom_validation/src/main/java/com/trieunt/controllers/PhoneController.java
@@ -0,0 +1,33 @@
+package com.trieunt.controllers;
+
+import com.trieunt.models.PhoneNumber;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
+public class PhoneController {
+    @GetMapping("/")
+    public String showForm(Model model){
+        System.out.println("hello");
+        model.addAttribute("phoneNumber", new PhoneNumber());
+        return "index";
+    }
+    @PostMapping("/submit")
+    public String checkValidation (@Valid @ModelAttribute("phoneNumber")PhoneNumber phoneNumber, BindingResult bindingResult, Model model){
+        new PhoneNumber().validate(phoneNumber, bindingResult);
+        if (bindingResult.hasFieldErrors()){
+            return "index";
+        }
+        else {
+            model.addAttribute("phoneNumber", phoneNumber.getNumber());
+            return "result";
+        }
+    }
+}
